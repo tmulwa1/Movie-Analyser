e3827bf978f70f29e92d6c73fd634061793cefb6
Tiffany Mulwa <tmulwa01@gmail.com>
2026-07-24 14:55:59 +0300

    Began creating Movie class in database file

---
+++ src/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine, Column, Integer, String, Float, Date, Text
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+Base = declarative_base()
+engine = create_engine('sqlite:///data/movies.db')
+Session = sessionmaker(bind=engine)
+
+class Movie(Base):
+    __tablename__ = 'movies'
+    
+    id = Column(Integer, primary_key=True)
+    tmdb_id = Column(Integer, nullable=False)
+    title = Column(String(255), nullable=False)
+    release_year = Column(String(4))
+    genres = Column(String(255))
+    director = Column(String(255))
+    runtime = Column(Integer)
+    poster_path = Column(String(255))
+    vote_average = Column(Float)
+    my_rating = Column(Float)
+    notes = Column(Text)
+    date_watched = Column(Date)
+
