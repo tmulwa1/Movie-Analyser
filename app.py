61990e17d7fe25b40cf389677be3a586d42dbc4a
Tiffany Mulwa <tmulwa01@gmail.com>
2026-07-27 09:26:52 +0300

    Completed add movie function

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from src.database import get_session, initialise_db, Movie
-from src.tmdb_client import search_movies
+from src.tmdb_client import get_movie_details, search_movies
 
 app = Flask(__name__)
 
@@ -14,6 +14,17 @@ def dashboard():
     session.close()
 
     return rendered
+
+@app.route('/add', methods=['GET','POST'])
+def add_movie():
+    # Initialize results to an empty list
+    results = []
+
+    if request.method == 'POST':
+        # Typed a title and searched for it
+        search = request.form.get('query')
+        results = search_movies(search)
+    return render_template('add_movie.html', results=results)
 
 if __name__ == '__main__':
     initialise_db()
