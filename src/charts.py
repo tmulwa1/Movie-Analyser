8471d6dda7f7a8adcb8da27263aa2f936c6a48f8
Tiffany Mulwa <tmulwa01@gmail.com>
2026-07-28 09:43:08 +0300

    Added a genre breakdown chart

---
+++ src/charts.py
@@ -0,0 +1,14 @@
+import os
+import matplotlib.pyplot as plt
+from src.stats import genre_breakdown
+
+def create_genre_chart():
+    # Creating the chart folder
+    os.makedirs('static/charts', exist_ok=True)
+    data = genre_breakdown()
+    data.plot.bar(title="Genre Breakdown")
+    # Rotates the labels to make them more readable
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.savefig('static/charts/genre_breakdown.png')
+    plt.close()
